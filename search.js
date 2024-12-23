5eb1a824339320237c56f188a32f50efaa4e523c
Ville V. Vanninen <ville@pumpula.net>
2024-12-23 19:14:57 +0200

    Simplify simplify simplify

--- search.js
+++ search.js
@@ -1,41 +1,44 @@
 /**
  * Init filterer
  * @param {node} listen - Where to listen for change & input events. Set to your radio group element if you want to filter to radios.
- * @param {node} scope - Target element that has its contents filtered.
- * @param {selector} item - Selector for items that get shown/hidden based on search.
- * @param {selector} match - Selector for search terms inside the items.
- * @param {function} callback - Callback that will be run after every search invocation. Called with found matches as its only parameter.
+ * @param {string} row - CSS Selector that targets the rows that you want to show or hide. Doesn't have to be a table row, any element will do. Example: `#mytable tbody tr`
+ * @param {string} match - Selector for elements inside your rows that contain your search terms. Example `:is(th, td)`
+ * @param {function} callback - Optional callback that will be run after every search invocation. Called with found shown items and matched terms
  */
-export function filterer ({listen, selectors: {scope, item, match}, callback}) {
-  const style = document.head.appendChild(document.createElement('style'))
 
-  // Mangle search attribute name from `match` selector.
-  const attribute = btoa(match).replace(/\+/g, 'plus').replace(/\//g, 'slash').replace(/=+$/, 'equals');
-
-  // Build search index
-  Array.from(document.querySelectorAll(`${scope} ${item} ${match}`)).forEach(el => {
-    el.setAttribute(attribute, el.textContent)
-  })
-
+export function filterer ({name, row, callback}) {
   const lazy = debounce(filter, 100);
-  listen.addEventListener('change', lazy)
-  listen.addEventListener('input', lazy)
+  const attribute = 'data-' + name
+  const style = document.head.appendChild(document.createElement('style'))
+  style.setAttribute(attribute, '')
 
-  // The actual filtering
-  function filter ({target: {value}}) {
-    const words = value.split(' ').map(CSS.escape).filter(t => t != "")
-    const has = words.map(word => `:has(${match}[${attribute}*="${word}" i])`).join('')
+  document.addEventListener('change', lazy)
+  document.addEventListener('input', lazy)
+
+
+  function filter (e) {
+    if (e.target.name !== name) return
+
+    const words = e.target.value.split(' ').map(CSS.escape).filter(t => t != "")
+
+    // Selectors
+    const has = words.map(word => `:has([${attribute}*="${word}" i])`).join('')
     const nothas = `:not(${has})`
-    const hide = `${scope} ${item}${nothas} {display: none}`
-    const hilites = words.map(word => `${item} ${match}[${attribute}*="${word}" i] {background: LemonChiffon}`)
+    const hide = `${row}${nothas}`
+    const hilites = words.map(word => `${row} [${attribute}*="${word}" i]`)
 
-    // If we wanted to highlight search terms.
-    style.innerHTML = hide + hilites.join('\n')
-    callback(document.querySelectorAll(`${scope} ${item}${has}`))
+    // Write actual css
+    style.innerHTML = hide + '{display: none}' + hilites.map(h => h + '{background: var(--hilite-bg)}')
+
+    if (callback) {
+      const rows = document.querySelectorAll(`${row}${has}`)
+      const matches = hilites.length && document.querySelectorAll(hilites.join(','))
+      callback(rows, matches)
+    }
   }
 }
 
-//This is like five lines so don't want this as an external dependency.
+//This is like five lines so I don't want this as an external dependency.
 function debounce (fn, delay) {
   let id;
   return function (...args) {
