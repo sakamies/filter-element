a895ce71beec00636e019548bfd5c24078935351
Ville V. Vanninen <ville@pumpula.net>
2024-12-25 21:08:10 +0200

    Refactoring and make checkboxes work

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 Where I try to have the browser do all the work for me.
 
-I'm using plain old css to do the filtering. I mean I build a selector to find matches to a search query and apply the selector to a &lt;style&gt; element. No iteration of any elements in javascript necessary. You might want to inform users of the number of matches, so there's an optional callback to get the number of matches.
+I'm using plain old (newly baseline) css to do the filtering. I mean I build a selector to find matches to a search query and apply the selector to a &lt;style&gt; element. No iteration of any elements in javascript necessary. You might want to inform users of the number of matches, so there's an optional callback to get the number of matches.
 
-Works for most inputs, but checkboxes don't work well yet.
+This is an excersise in using native DOM scripting and CSS, so I'm making some assumptions about the use of html.
+
+- Inputs need to be inside a form element.
+- Forms and inputs must have a name attribute.
+- Only string matching, numeric inputs will be compared a strings.
 
 In the example code, the search index is built with javascript. Search indexing should be rendered into the html already from the server, but this bit of javascript is to illustrate building a search index so I don't have to hand code it into this example.
-
-Would it be more performant to have some separate search index and set the hidden attribute on the searched items or something like that? I don't know, maybe, but this feels more browsery and browsers are super optimized for css!
--- filter.js
+++ filter.js
@@ -1,34 +1,68 @@
 /**
  * Init filterer
- * @param {node} listen - Where to listen for change & input events. Set to your radio group element if you want to filter to radios.
- * @param {string} row - CSS Selector that targets the rows that you want to show or hide. Doesn't have to be a table row, any element will do. Example: `#mytable tbody tr`
- * @param {string} match - Selector for elements inside your rows that contain your search terms. Example `:is(th, td)`
+ * @param {node} form - Optional form name to scope event listeners to.
+ * @param {string} selector - CSS Selector that targets the items that you want to show or hide based on the . Doesn't have to be a table row, any element will do. Example: `#mytable tbody tr`
  * @param {function} callback - Optional callback that will be run after every search invocation. Called with found shown items and matched terms
  */
 
-export function filterer ({name, row, callback}) {
-  const attribute = 'data-' + name
-  const style = document.head.appendChild(document.createElement('style'))
+// So it's easy to change the attribute prefix everywhere
+const attr = (name) => 'data-' + name
 
-  const lazy = debounce(filter, 100);
-  document.addEventListener('click', lazy)
-  document.addEventListener('change', lazy)
-  document.addEventListener('input', lazy)
+export function filterer ({form, table, row = 'tbody td', callback}) {
+  form = document.forms[form] || document.forms[0]
 
-  function filter (e) {
-    if (e.target.name !== name) return
+  //Flatten multiple same name values in form data to a single stylesheet per unique name
+  let styles = {}
+  Array.from(form.querySelectorAll('[name]')).forEach(({name}) => {styles[name] = null})
+  for (const name in styles) {
+    const attribute = attr(name)
+    styles[name] = document.head.appendChild(document.createElement('style'))
+    styles[name].setAttribute(attribute, '')
+  }
 
-    const words = e.target.value.split(' ').map(CSS.escape).filter(x => x != "")
+  const style = (name, content) => styles[name].innerHTML = content
+
+  //Setup events
+  const lazy = debounce(event, 100);
+  form.addEventListener('change', lazy)
+  form.addEventListener('input', lazy)
+
+  function event() {
+    const filters = {}
+    const data = new FormData(form)
+    //When there's multiple inputs with the same name, their values go under one key, but as a combined space delimited string of values. This makes sure multiple checkboxes with the same name are treated exactly the same as a string with spaces in a text input.
+    data.entries().forEach(([name]) => filters[name] = '')
+    data.entries().forEach(([name, value]) => filters[name] += value + ' ')
+
+    for (const name in styles) {
+      style(name, '')
+    }
+
+    for (name in filters) {
+      filter(name, filters[name])
+    }
+  }
+
+  //TODO: make a custom element that takes the args like <filter-able form="form_name_here" rows="optional selector, defauls is this.querySelectorAll('tbody tr')">
+  //Set hidden attribute for rows, so they can come from server pre rendered
+  //Use url search params for filters, so they can be given in a url, persist over reloads and the form can be submitted to the server and server can render the table if needed. Progressive enhancement for the win!
+
+  function filter (name, value) {
+    value = value.trim()
+    if (!value) return
+
+    const attribute = attr(name)
+    const words = value.split(' ').map(CSS.escape)
     const has = words.map(word => `:has([${attribute}*="${word}" i])`).join('')
     const nothas = `:not(${has})`
-    const hide = `${row}${nothas}`
-    const hilites = words.map(word => `${row} [${attribute}*="${word}" i]`)
+    const hide = `${table} ${row}${nothas}`
+    const hilites = words.map(word => `${table} ${row} [${attribute}*="${word}" i]`)
 
-    style.innerHTML = hide + '{display: none}' + hilites.map(h => h + '{background: var(--hilite-bg)}')
+    style(name, hide + '{display: none}' + hilites.map(h => h + '{background: var(--hilite-bg)}'))
 
     if (callback) {
-      const rows = document.querySelectorAll(`${row}${has}`)
-      const matches = hilites.length && document.querySelectorAll(hilites.join(','))
+      const rows = document.querySelectorAll(`${table} ${row}${has}`)
+      const matches = hilites && document.querySelectorAll(hilites.join(','))
       callback(rows, matches)
     }
   }
