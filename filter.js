7ea60b2b0339cc268f87c30ac456949471740a86
Ville V. Vanninen <ville@pumpula.net>
2024-12-29 02:03:59 +0200

    Move selectors to their own object

--- filter.js
+++ filter.js
@@ -1,102 +1,96 @@
 /**
- * Init filterer
+ * Custom element to filter contents inside it based on a form.
+ * @module Filter
  * @param {node} form - Optional form name to scope event listeners to.
  * @param {string} selector - CSS Selector that targets the items that you want to show or hide based on the . Doesn't have to be a table row, any element will do. Example: `#mytable tbody tr`
  * @param {function} callback - Optional callback that will be run after every search invocation. Called with found shown items and matched terms
  */
 
+let style
+const sel = {
+  tag: '',
+  form: '',
+  rows: 'tbody tr',
+  attr: (name, value) => `[data-${sel.tag}-${name}*="${value}" i]`,
+  has: str => `:has(${str})`,
+  is: str => `:is(${str})`,
+  not: str => `:not(${str})`,
+}
+
 export class Filter extends HTMLElement {
-  static observedAttributes = ['form', 'row']
+  static observedAttributes = ['form', 'rows']
 
-  attribute = (name) => 'data-' + this.localName + '-' + name
-  has = (selector) => `:has(${selector})`
-  is = (selector) => `:is(${selector})`
-  not = (selector) => `:not(${selector})`
-
-  #form
   get form() {
-    return document.forms[this.#form]
+    return document.forms[sel.form]
     || this.querySelector('form')
     || document.forms[0]
-    || console.warn('No form found for ' + this.localName)
+    || console.warn('No form found for ' + sel.tag)
   }
 
-  #rows = 'tbody tr'
   get rows() {
-    return this.querySelectorAll(this.#rows)
+    return this.querySelectorAll(sel.rows)
   }
 
   constructor() {
     super()
+    sel.tag = this.localName
+    style = style || this.appendChild(document.createElement('style'))
     const lazy = debounce(this.filter.bind(this), 100)
     this.form && this.form.addEventListener('input', lazy)
   }
 
   attributeChangedCallback(name, _, value) {
-    if (name === 'form') {this.#form = value}
-    if (name === 'row') {this.#rows = value}
+    if (name === 'form') {sel.form = value}
+    if (name === 'rows') {sel.rows = value}
   }
 
   filter() {
-    const rows = Array.from(this.querySelectorAll(this.#rows))
-    let data = Array.from(new FormData(this.form))
-    data = data.filter(([_, value]) => value)
+    const rows = this.querySelectorAll(sel.rows)
+    const data = Array.from(new FormData(this.form)).filter(([_, v]) => v)
+    const selector = data.flatMap(this.selectBy).join('')
 
-    let select = data.flatMap(([name, value]) => this.selectBy(name, value))
-    select = select.join('')
-
-    let found = this.querySelectorAll(`${this.#rows}${select}`)
+    const found = this.querySelectorAll(sel.rows+selector)
     if (!this.dispatch(found)) return
 
-    found = Array.from(found)
-    for (const row of rows) {
-      row.hidden = !found.includes(row)
-    }
+    const shown = Array.from(found)
+    rows.forEach(row => row.hidden = !shown.includes(row))
   }
 
   dispatch(found) {
-    const event = new CustomEvent(
-      this.localName,
-      {
-        cancelable: true,
-        bubbles: true,
-        detail: {found},
-      }
-    )
+    const event = new CustomEvent(sel.tag, {
+      cancelable: true,
+      bubbles: true,
+      detail: {found},
+    })
     return this.dispatchEvent(event)
   }
 
-  selectBy(name, value, not = '') {
-    [name, not] = name.split(/(?=:)/)
+  selectBy([name, value], flag) {
+    [name, flag] = name.split(':')
+    name = CSS.escape(name)
 
-    const attr = this.attribute(name)
     const words = value.trim().split(' ').map(CSS.escape)
-    const attrs = words.map(word => `[${attr}*="${word}" i]`)
+    const attrs = words.map(word => sel.attr(name, word))
+    const has = attrs.map(sel.has).join('')
 
-    const has = attrs.map(a => this.has(a)).join('')
-    return not === ':not' && this.not(has) || has
+    return flag === 'not' && sel.not(has) || has
   }
 
-  #style
-  hilite(name, value, hilite = '') {
-    this.#style = this.#style || this.appendChild(document.createElement('style'))
-    if (!is) {this.#style.innerHTML = ''}
+  hilite([name, value], flag) {
+    [name, flag] = name.split(':')
+    if (flag !== 'hi') return
 
-    [name, hilite] = name.split(/(?=:)/)
-    if (hilite !== ':hilite') return
-
-    const attr = this.attribute(name)
+    name = CSS.escape(name)
     const words = value.trim().split(' ').map(CSS.escape)
-    const attrs = words.map(word => `[${attr}*="${word}" i]`)
-
-    const is = this.is(attrs.join(''))
+    const attrs = words.map(word => sel.attr(name, word))
+    const is = sel.is(attrs.join(''))
 
     //TODO: how to scope these styles to only this instance of this element?
     //TODO: Should probably think through what kind of specificity this selector should have, or use something else than <style> for highlighting matches.
-    const selector = `${this.localName} ${this.#rows} ${is}`
-    const text = `var(--${this.localName}-marktext, MarkText)`
-    const mark = `var(--${this.localName}-mark, Mark)`
-    this.#style.innerHTML = `${selector} {color: ${text}; background: ${mark};`
+    const selector = `${sel.tag} ${sel.rows} ${is}`
+    const text = `var(--${sel.tag}-marktext, MarkText)`
+    const mark = `var(--${sel.tag}-mark, Mark)`
+    style.innerHTML = `${selector} {color: ${text}; background: ${mark};`
   }
 }
 
