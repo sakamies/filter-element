2cfbf3c1f3686bfbf3d84f9ad5e2dc1d7486a2bd
Ville V. Vanninen <ville@pumpula.net>
2024-12-31 16:27:35 +0200

    Get working with multiple element instances

--- filter.js
+++ filter.js
@@ -5,52 +5,43 @@
  * @param {string} rows - CSS Selector that targets the items that you want to show or hide based on the form. Doesn't have to be a table row, any element will do. Example: `#mytable .card`
  */
 
-let style
-const sel = {
-  tag: '',
-  form: '',
-  rows: 'tbody tr',
-  attr: (name, value) => `[data-${sel.tag}-${name}*="${value}" i]`,
-  has: str => `:has(${str})`,
-  is: str => `:is(${str})`,
-  not: str => `:not(${str})`,
-}
-
 export class Filter extends HTMLElement {
   static observedAttributes = ['form', 'rows']
+  //TODO: attribute like "exact" or something to have only exact matches?
+
+  styleElement
+  selectors = {
+    attr: (name, value) => `[data-${this.localName}-${name}*="${value}" i]`,
+    has: str => `:has(${str})`,
+    is: str => `:is(${str})`,
+    not: str => `:not(${str})`,
+  }
 
   get form() {
-    return document.forms[sel.form]
+    return document.forms[this.getAttribute('form')]
     || this.querySelector('form')
+    || this.closest('form')
     || document.forms[0]
     || console.warn('No form found for', this)
   }
 
   get rows() {
-    return this.querySelectorAll(sel.rows)
+    return this.querySelectorAll(this.rowsSelector)
   }
 
   constructor() {
     super()
-    sel.tag = this.localName
-    style = style || this.appendChild(document.createElement('style'))
+    this.styleElement = this.styleElement || this.appendChild(document.createElement('style'))
     const lazy = debounce(this.filter.bind(this), 100)
     this.form && this.form.addEventListener('input', lazy)
-
-    // (new MutationObserver(lazy)).observe(this, {
-    //   childList: true,
-    //   subtree: true,
-    //   attributes: true,
-    // })
   }
 
   attributeChangedCallback(name, _, value) {
-    sel[name] = value
     this.filter()
   }
 
   dispatch(found) {
-    const event = new CustomEvent(sel.tag, {
+    const event = new CustomEvent(this.localName, {
       cancelable: true,
       bubbles: true,
       detail: {found},
@@ -59,30 +50,34 @@ export class Filter extends HTMLElement {
   }
 
   filter() {
-    const rows = this.querySelectorAll(sel.rows)
+    const rows = this.querySelectorAll(this.rowsSelector)
     const data = Array.from(new FormData(this.form)).filter(([_, v]) => v)
     const rowHas = data.map(this.hasAttributeSelectors, this).join('')
 
-    const found = this.querySelectorAll(sel.rows+rowHas)
+    const found = this.querySelectorAll(this.rowsSelector+rowHas)
     if (!this.dispatch(found)) return
 
     const shown = Array.from(found)
     rows.forEach(row => row.hidden = !shown.includes(row))
 
     this.hilite(data.flatMap(this.hiliteSelectors, this).join(','))
+
+  //TODO: If this is to be a generic filter and not a table specific filter, the default should be to filter direct children instead of table rows.
+  get rowsSelector() {
+    return this.getAttribute('rows') || 'tbody tr'
   }
 
   attributeSelectors(name, value) {
     name = CSS.escape(name)
     const words = value.trim().split(' ').map(CSS.escape)
-    let attrs = words.map(word => sel.attr(name, word))
+    let attrs = words.map(word => this.selectors.attr(name, word))
     return attrs
   }
 
   hasAttributeSelectors([name, value], flag) {
     [name, flag] = name.split(':')
-    const selector = this.attributeSelectors(name, value).map(sel.has).join('')
-    return flag === 'not' && sel.not(selector) || selector
+    const selector = this.attributeSelectors(name, value).map(this.selectors.has).join('')
+    return flag === 'not' && this.selectors.not(selector) || selector
   }
 
   hiliteSelectors([name, value], flag) {
@@ -92,10 +87,14 @@ export class Filter extends HTMLElement {
 
   hilite(attrsToHilite) {
     // TODO: this is really simple, but using Range() and ::highlight() would be more accurate. Not sure if it's worth the complexity.
-    const selector = `${sel.rows} ${sel.is(attrsToHilite)}`
-    const text = `var(--${sel.tag}-marktext, MarkText)`
-    const mark = `var(--${sel.tag}-mark, Mark)`
-    style.innerHTML = `@scope {${selector} {color: ${text}; background-color: ${mark};}`
+    if (attrsToHilite && this.id) {
+      const selector = `#${this.id} ${this.rowsSelector} ${this.selectors.is(attrsToHilite)}`
+      const text = `var(--${this.localName}-marktext, MarkText)`
+      const mark = `var(--${this.localName}-mark, Mark)`
+      this.styleElement.innerHTML = `${selector} {color: ${text}; background-color: ${mark};`
+    } else {
+      this.styleElement.innerHTML = ''
+    }
   }
 }
 
